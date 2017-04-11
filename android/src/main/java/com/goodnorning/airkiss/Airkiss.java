0577d16431bf3397d1044f9563e361b66503b936
linsht <scott.lin@viewmobile.com>
2017-04-11 19:00:31 +0800

    debug airkiss for android.

--- android/src/main/java/com/goodnorning/airkiss/Airkiss.java
+++ android/src/main/java/com/goodnorning/airkiss/Airkiss.java
@@ -4,11 +4,11 @@ import android.content.Context;
 import android.support.annotation.Nullable;
 import android.util.SparseArray;
 import android.os.AsyncTask;
+import android.util.Base64;
 
 import com.facebook.common.logging.FLog;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.GuardedAsyncTask;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -27,13 +27,12 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
-
 import android.util.Log;
 
 public final class Airkiss extends ReactContextBaseJavaModule {
     private static final String TAG = "Airkiss";
     private AirKissTask task1;
-    private MyTask task2;
+    private MyTask task2 = null;
 
     public Airkiss(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -45,20 +44,23 @@ public final class Airkiss extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void start(final String ssid, final String password, final Callback callback1, final Callback callback2) {
-    	task1 = new AirKissTask(callback1, new AirKissEncoder(ssid, password));
-        task2 = new MyTask(callback2);
+    public void start(final String ssid, final String password, final Callback callback) {
+    	task1 = new AirKissTask(callback,new AirKissEncoder(ssid, password));
         task1.execute();
+    }
+
+    @ReactMethod
+    public void startGetDeviceInfo(final Callback callback){
+        task2 = new MyTask(callback);
         task2.execute();
     }
 
 	@ReactMethod
     public void stop() {
     	task1.stop();
-    	task2.stop();
+        if (task2 != null)
+    	   task2.stop();
     }
-
-
 
 	private class MyTask extends AsyncTask<Void,Void,Void> {
         private static final int PORT1 = 12476;
@@ -69,6 +71,8 @@ public final class Airkiss extends ReactContextBaseJavaModule {
         private static final int AIRKISS_LAN_SSDP_NOTIFY_CMD = 0x1002;  
         private Callback mCallback;
         private boolean mStop = false;
+        private volatile boolean mGotNotify = false;
+        private String mDevice;
 
         public MyTask(final Callback callback) {
         	mCallback = callback;
@@ -78,30 +82,26 @@ public final class Airkiss extends ReactContextBaseJavaModule {
         protected void onPreExecute() {
             new Thread(new Runnable() {
               public void run() {
-                    byte[] buffer = new byte[200];
+                    byte[] buffer = new byte[15000];
                     try {
                         DatagramSocket udpServerSocket = new DatagramSocket(12476);
                         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-                        // int replyByteCounter = 0;
                         udpServerSocket.setSoTimeout(1000);
                         while (true) {
-                            // if (getStatus() == Status.FINISHED) {
-                            //     System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
-                            //     break;
-                            // }
+                            if (getStatus() == Status.FINISHED) {
+                                System.out.println("@@@@@@@@@@@@@@@@@@@@@@");
+                                break;
+                            }
                         	if (mStop)
                         		break;
                             try {
                                 udpServerSocket.receive(packet);
                                 byte receivedData[] = packet.getData();
                                 System.out.println("==========================::"+packet.getLength());
-                                for (byte b : receivedData) {
-                                    System.out.println(b);
-                                }
-                                // byte[] inBuff = new byte[200];
-                                // System.out.println("==========================::"+ new String(inBuff, 0 , packet.getLength())); 
-                                if (parseNotifyData(receivedData))
+                                if (parseNotifyData(receivedData,packet.getLength())) {
+                                    mGotNotify = true;
                                     break;
+                                }
                             } catch (SocketTimeoutException e) {
                                 e.printStackTrace();
                             } catch (IOException e) {
@@ -117,18 +117,17 @@ public final class Airkiss extends ReactContextBaseJavaModule {
             }).start();
         }
 
-    	private boolean parseNotifyData(byte[] data) {
+    	private boolean parseNotifyData(byte[] data, int length) {
             Log.d("parseNotifyData","----------");
             byte[] newData = new byte[200];
-            int length = data.length;
             int count = 0;
             boolean flag = false;
             Log.d("parseNotifyData","length:"+length);
             while(count<length-4 && length<200){
-                byte num0 = (byte)(data[count++]&0xFF);
-                byte num1 = (byte)(data[count++]&0xFF);
-                byte num2 = (byte)(data[count++]&0xFF);
-                byte num3 = (byte)(data[count++]&0xFF);
+                byte num0 = data[count++];
+                byte num1 = data[count++];
+                byte num2 = data[count++];
+                byte num3 = data[count++];
                 System.out.println(num0+""+num1+""+num2+""+num3);
                 if (num0 == -3 && num1 == 1 && num2 == -2 && num3 == -4) {
                     Log.d("parseNotifyData","find magic num");
@@ -137,14 +136,29 @@ public final class Airkiss extends ReactContextBaseJavaModule {
                     break;
                 }
             }
-            count = 0;
-            length = length-count;
+
             if (flag) {
+                count = 0;
+                length = length-count;
                 while(count<length-2&&length>2) {
                     int cmd = (newData[count++]<<8|newData[count++])&0xFFFF;
                     if(cmd == AIRKISS_LAN_SSDP_NOTIFY_CMD) {
                         Log.d("parseNotifyData","got device data");
-                        mCallback(true)
+                        byte[] buffer = new byte[200];
+                        for (int i=0;i<200;i++) {
+                            buffer[i]=0;
+                        }
+                        int len = length-count;
+                        System.out.println(len);
+                        System.arraycopy(newData,count,buffer,0,len);
+                        // for (int i=0;i<len;i++) {
+                        //     // System.out.println(buffer[i]);
+                        //     if (buffer[i]>0x7F)
+                        //         buffer[i]='\0';
+                        // }
+                    
+                        mDevice = new String(buffer,0,len);
+                        Log.d("parseNotifyData------",mDevice);
                         return true;
                     }           
                 }
@@ -154,8 +168,16 @@ public final class Airkiss extends ReactContextBaseJavaModule {
 
         @Override
         protected Void doInBackground(Void... params) {
- 
-
+            Log.d("parseNotifyData------","doInBackground");
+            int count = 0;
+            while(!mGotNotify && count < 60) {
+                count++;
+                try {  
+                    Thread.sleep(500);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
             return null;
         }
 
@@ -165,7 +187,15 @@ public final class Airkiss extends ReactContextBaseJavaModule {
 
         @Override
         protected void onPostExecute(Void params) {
-      
+            Log.d("parseNotifyData------","onPostExecute :"+mGotNotify);
+            WritableMap result = Arguments.createMap();
+            if (mGotNotify) {
+                result.putInt("code",1);
+                result.putString("device",mDevice);
+            } else {
+                result.putInt("code",-1);
+            }
+            mCallback.invoke(result);
         }  
         public void stop() {
         	mStop = true;
@@ -185,7 +215,7 @@ public final class Airkiss extends ReactContextBaseJavaModule {
         private volatile boolean mDone = false;
         private boolean mStop = false;
 
-        public AirKissTask(final Callback callback, AirKissEncoder encoder) {
+        public AirKissTask(final Callback callback,AirKissEncoder encoder) {
             mCallback = callback;
             mRandomChar = encoder.getRandomChar();
             mAirKissEncoder = encoder;
@@ -252,7 +282,6 @@ public final class Airkiss extends ReactContextBaseJavaModule {
         protected Void doInBackground(Void... params) {
             try {
                 mSocket = new DatagramSocket();
-                
                 mSocket.setBroadcast(true);
             } catch (Exception e) {
                 e.printStackTrace();
@@ -266,7 +295,6 @@ public final class Airkiss extends ReactContextBaseJavaModule {
                         return null;
                 }
             }
-
             return null;
         }
 
@@ -276,7 +304,13 @@ public final class Airkiss extends ReactContextBaseJavaModule {
 
         @Override
         protected void onPostExecute(Void params) {
- 
+            WritableMap result = Arguments.createMap();
+            if (mDone) {
+                result.putInt("code",1);
+            } else {
+                result.putInt("code",-1);
+            }
+            mCallback.invoke(result);
         }
 
         public void stop() {
--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 'use strict';
 
 import { DeviceEventEmitter, NativeModules, Platform } from 'react-native';
-let Airkiss = NativeModules.Airkiss;
+var airkiss = NativeModules.Airkiss;
 
 class Airkiss {
 
-  start(ssid,password,callback1,callback2) {
-    Airkiss.start(ssid,password,(ret)=>{
-      callback1(ret)
-    },ret=>{
-      callback2(ret)
+  start(ssid,password,callback) {
+    airkiss.start(ssid,password,(ret)=>{
+      callback(ret)
+    })
+  }
+  startGetDeviceInfo(callback) {
+    airkiss.startGetDeviceInfo(ret=>{
+      callback(ret)
     })
   }
   stop(){
-    Airkiss.stop()
+    airkiss.stop()
   }
 
   
